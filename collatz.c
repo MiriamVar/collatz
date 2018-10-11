2069cf06ea7bb84c306e5780d621dea2d9c501c8
MiriamVar <mimika648@gmail.com>
2018-10-11 15:05:22 +0200

    nejde

--- collatz.c
+++ collatz.c
@@ -1,17 +1,41 @@
 #include <stdio.h>
 
 int main(int argc, char const *argv[]) {
-  int num = 1;
-  int count = 0; //Use only for task 4.1, 4.2
-
+  int num,i;
+  int count = 0;
+   //Use only for task 4.1, 4.2 
   /*Read input from user into variable num*/
-
   /*Input validation*/
-
-  while (num != 1) {  //main cycle
-    /* code */
-    
-    
-  }
+	//printf("Type a number: ");
+	//scanf("%d", &num);
+	//if(num<1 && num>1000)
+	//	{
+	//		printf("You typed wrong number.");
+	//		return 1;
+	//	}
+	for(num=100;num<201;num++)
+	{
+		count = 0;
+		//num =i;
+    	while (num != 1)
+		{  
+    		if(num%2==0)
+			{
+				num=num/2;
+				//printf("%d ", num);
+				count+=1;
+			}
+			else
+			{
+				num= num*3+1;
+				//printf("%d ", num);
+				count+=1;
+			}
+    	}
+    	printf("\n%d", count);
+		//printf("\n%d", count);
+	}
+	
+	
   return 0;
 }
