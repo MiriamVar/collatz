8f30c794636c02b3a2d5a03000de6ca28d5a0f3b
michal.novotny <michal.novotny@akademiasovy.sk>
2018-10-11 15:43:07 +0200

    ide :P

--- collatz.c
+++ collatz.c
@@ -3,7 +3,7 @@
 int main(int argc, char const *argv[]) {
   int num,i;
   int count = 0;
-   //Use only for task 4.1, 4.2 
+   //Use only for task 4.1, 4.2
   /*Read input from user into variable num*/
   /*Input validation*/
 	//printf("Type a number: ");
@@ -13,29 +13,23 @@ int main(int argc, char const *argv[]) {
 	//		printf("You typed wrong number.");
 	//		return 1;
 	//	}
-	for(num=100;num<201;num++)
-	{
+	for(i=100;i<201;i++){
+    num = i;
 		count = 0;
 		//num =i;
-    	while (num != 1)
-		{  
+    	while (num != 1) {
     		if(num%2==0)
 			{
 				num=num/2;
-				//printf("%d ", num);
-				count+=1;
+			}	else {
+				num=num*3+1;
 			}
-			else
-			{
-				num= num*3+1;
-				//printf("%d ", num);
-				count+=1;
-			}
-    	}
-    	printf("\n%d", count);
+      count++;
+    }
+    	printf("\n%d %d", i, count);
 		//printf("\n%d", count);
 	}
-	
-	
+
+
   return 0;
 }
